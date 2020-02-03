fdfcdb4ee50f328c62ec418d81e24a08ac384555
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-02-03 21:49:08 +0530

    Create InsertionLinkedList.py

---
+++ InsertionLinkedList.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def print_linkedlist(self):
+        curr_node=self.head
+        while curr_node:
+            print(curr_node.data,end=" ")
+            print("->",end=" ")
+            curr_node=curr_node.next
+        print("NULL_PTR")
+    def prepend(self,data):
+        new_node=Node(data)
+        curr=self.head
+        new_node.next=self.head
+        self.head=new_node
+    def insertAfterNode(self,data,prev_node):
+        if not prev_node:
+            print("Prevnode is not present in the list")
+            return
+        new_node=Node(data)
+        new_node.next=prev_node.next
+        prev_node.next=new_node
+        
+        
+    def append(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        last_node=self.head
+        while last_node.next:
+            last_node=last_node.next
+        last_node.next=new_node
+llist=LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+llist.append("E")
+llist.prepend("O")
+llist.insertAfterNode("M",llist.head.next)
+llist.print_linkedlist()
