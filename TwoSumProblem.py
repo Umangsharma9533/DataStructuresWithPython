18c29880ea380786f462c6e8ab01b281925728b2
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-02-16 15:33:38 +0530

    Update TwoSumProblem.py

--- TwoSumProblem.py
+++ TwoSumProblem.py
@@ -3,6 +3,8 @@ n=int(input("ENter the number of index required"))
 for i in range(0,n):
     ele=int(input())
     A.append(ele)
+#size complexity = O(n)
+#time complexity = O(n)
 def twoSumProblem(A,target):
     ht=dict()
     for i in range(len(A)):
@@ -12,5 +14,20 @@ def twoSumProblem(A,target):
         else:
             ht[target-A[i]]=A[i]
     return False
+#Size=O(1)
+#Time O(n)
+def twoSumProblemOpt(A,target):
+    i=0
+    j=len(A)-1
+    while i<=j:
+        if (A[i]+A[j])==target:
+            print(A[i],A[j])
+            return True
+        elif  (A[i]+A[j])<target:
+            i+=1
+        else:
+            j-=1
+    return False
 target=int(input("Enter the target"))
+
 print(twoSumProblem(A,target))
